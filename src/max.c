55ef669c15bd03b288cd8ff19ca681cc919f0f3e
MikkelHojbjerg <89967176+MikkelHojbjerg@users.noreply.github.com>
2022-10-10 11:38:10 +0200

    Aflevering Færdig

--- src/max.c
+++ src/max.c
@@ -3,18 +3,21 @@
 
 int max(int* numbers, int size)
 {
-
+	//Assert hvis size er mindre end nul
 	assert(!(size < 0));
 
 	int res = numbers[0];
 
+	//Hvis der kun er en værdi i arrayet er værdien jo automatisk det største
 	if(size == 1){
 		return res;
 	}
 
-
+	//Loop kører hele arrayet igennem
 	for(int i = 1; i < size; i++){
+		//Tjekker om nuværende nummer i arrayet er større end det nuværende nummer
 		if(numbers[i] > res){
+			//Hvis ja bliver res det nye største tal
 			res = numbers[i];
 		}
 	}
